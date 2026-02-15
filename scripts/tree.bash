a876255e414b850fe145f1f5658fba8f7ba39bf2
Alex <54222750+linguisticmind@users.noreply.github.com>
2026-02-15 05:44:04 +0000

    A fix to the `tree` script: use `--noreport` instead of piping to `head -n -2`.

--- scripts/tree.bash
+++ scripts/tree.bash
@@ -14,6 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# version: 0.1.0
+# version: 0.1.1
 
-tree -n ${WATCHTOWER_SCRIPT[color]:+-C} "$@" | head -n -2
+tree -n ${WATCHTOWER_SCRIPT[color]:+-C} --noreport "$@"
